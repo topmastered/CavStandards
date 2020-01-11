db71067d25f5dee4d34e614e6c1905fca04fa158
Bryce Cary <bcary.md@gmail.com>
2020-01-10 19:47:22 -0500

    v0.0.3

--- source/website/siteConfig.js
+++ source/website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
     title: "4541 Cavineers", // Title for your website.
     tagline: "Code Standards",
     url: "https://your-docusaurus-test-site.com", // Your website URL
-    baseUrl: "/", // Base URL for your project */
+    baseUrl: "/CodeStandards", // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
     //   baseUrl: '/test-site/',
