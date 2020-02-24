78a23d52db5040c19e2b8f1ab63afa6c663ea175
Bryce Cary <bcary.md@gmail.com>
2020-02-23 22:11:41 -0500

    Added build script, README, and contributing guidelines

---
+++ README.md
@@ -0,0 +1,15 @@
+# CodeStandards
+
+CodeStandards is an open source code standards repository created and maintained by [FRC Team 4541](https://github.com/cavineers).
+
+## Visiting the website
+
+[Website](https://cavineers.github.io/CodeStandards)
+
+## Contributing
+
+Contributions are open to everyone!
+
+You can visit our [announcement](https://cavineers.github.io/CodeStandards/blog/2020/1/10/contributing.html) about contributing or our [documentation](https://cavineers.github.io/CodeStandards/docs/contributing).
+
+These standards are far from complete and we would love your help!
--- source/website/siteConfig.js
+++ source/website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
     title: "4541 Cavineers", // Title for your website.
     tagline: "Code Standards",
-    url: "https://your-docusaurus-test-site.com", // Your website URL
+    url: "https://cavineers.github.io", // Your website URL
     baseUrl: "/CodeStandards/", // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -31,7 +31,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: "code-standards",
-    organizationName: "facebook",
+    organizationName: "4541 Cavineers",
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
